e4980b6be76516fff2a57ca5bdaf4b1048a79ca6
Kirill Grinkov <79636456163@yandex.ru>
2023-06-19 11:29:14 +0400

    addtask13

---
+++ task13/task13.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.Clear();
+//Console.Write("Введите число: ");
+//string userImput = Console.ReadLine() ??"";
+//int num = int.Parse(userImput);
+
+
+int Prompt (string message)
+{
+Console.Write (message) ;
+string value = Console. ReadLine () ;
+int result = Convert. ToInt32 (value);
+return result;
+}
+int GetThirdRank (int number)
+{
+    while (number > 999)
+{
+number /= 10;
+}
+    return number % 10;
+}
+bool ValidateNumber (int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine ("Третьей цифры нет, повторите ввод") ;
+        return false;
+    }
+    return true;
+}
+int number = Prompt ("Введите число: ");
+if (ValidateNumber (number) )
+{
+Console.WriteLine (GetThirdRank (number) ) ;
+}
---
+++ task15/task15.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.Clear();
+Console.Write("Введите цифру дня недели: ");
+string userImput = Console.ReadLine() ??"";
+int weekDay = int.Parse(userImput);
+
+if (weekDay < 1 || weekDay >7)
+{
+    Console.WriteLine("Такого дня недели нет! ");
+}
+else if (weekDay >= 1 && weekDay <= 5)
+{
+    Console.WriteLine("Будний день((");
+}
+else if (weekDay >= 6 && weekDay <= 7)
+{
+    Console.WriteLine("Выходной!!");
+}
